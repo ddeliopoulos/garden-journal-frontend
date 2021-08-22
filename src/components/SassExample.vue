7505433645a88cc9b92dd08a7abb2ec11ba39c6a
Paweł Szymański <pawelfiu@gmail.com>
2021-08-22 11:21:17 +0200

    Add SASS

---
+++ src/components/SassExample.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "SassExample"
+}
+</script>
+
+<template>
+<div id="hello-sass">Hello SASS!</div>
+</template>
+
+<style scoped lang="scss">
+  #hello-sass {
+    color: green
+  }
+</style>
